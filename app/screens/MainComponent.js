93e6f3c677422cbaf0fbca09f8b5b8e8b40d9f46
Jayden-58 <jayden.kellar@gmail.com>
2023-01-22 10:08:54 -0800

    added icons to navbar

--- app/screens/MainComponent.js
+++ app/screens/MainComponent.js
@@ -1,8 +1,9 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import HomeScreen from "./HomeScreen";
 import AddInventoryScreen from "./AddInventoryScreen";
 import AccountScreen from "./AccountScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
 
 const Tab = createBottomTabNavigator();
 
@@ -32,21 +33,57 @@ const Main = () => {
         <View style={{ flex: 1, backgroundColor: "#888" }}>
             <Tab.Navigator
                 initialRouteName='Home'
+                options={{
+                    showIcon: true
+                }}
             >
                 <Tab.Screen
                     name='Home'
                     component={HomeScreen}
-                    options={{ title: 'Home' }}
+                    options={{ 
+                        title: 'Home',
+                        tabBarIcon: ({ color }) => (
+                            <Icon
+                                name='home'
+                                type='ionicon'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
                 />
                 <Tab.Screen
                     name='Add Inventory'
                     component={AddInventoryScreen}
-                    options={{ title: 'Add Inventory' }}
+                    options={{ 
+                        title: 'Add Inventory',
+                        tabBarIcon: ({ color }) => (
+                            <Icon
+                                name='add-outline'
+                                type='ionicon'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
                 />
                 <Tab.Screen
                     name='Account'
                     component={AccountScreen}
-                    options={{ title: 'Account' }}
+                    options={{ 
+                        title: 'Account',
+                        tabBarIcon: ({ color }) => (
+                            <Icon
+                                name='person-outline'
+                                type='ionicon'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
                 />
             </Tab.Navigator>
         </View>
