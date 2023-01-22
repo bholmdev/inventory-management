5a2e43e284106a03f0eadfb87a3337678caacece
Brandon Holman <bholmdev@gmail.com>
2023-01-22 11:06:09 -0600

    Removed Formik

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -59,23 +59,6 @@ const AccountScreen = () => {
           <View>
             <View>
               <Text>Future Login Screen!</Text>
-              <Formik>
-                <Formik
-                  initialValues={{ email: '' }}
-                  onSubmit={values => console.log(values)}
-                >
-                  {({ handleChange, handleBlur, handleSubmit, values }) => (
-                    <View>
-                      <TextInput
-                        onChangeText={handleChange('email')}
-                        onBlur={handleBlur('email')}
-                        value={values.email}
-                      />
-                      <Button onPress={handleSubmit} title="Submit" />
-                    </View>
-                  )}
-                </Formik>
-              </Formik>
               <Button
                 onPress={() => setLoginModalVisible(!loginModalVisible)}
                 title="Close Modal"
